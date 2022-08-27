f3d5cb0ee2eb3b96e2a98f9c100920eae815c6df
Bruno Viana Rufino <61715911+ViFeBs@users.noreply.github.com>
2022-08-26 21:54:57 -0300

    Update FolhaDePagamento.java

--- Nova pasta/aula03/Exercicio/funcionarios/src/FolhaDePagamento.java
+++ Nova pasta/aula03/Exercicio/funcionarios/src/FolhaDePagamento.java
@@ -5,7 +5,11 @@ public class FolhaDePagamento {
     public void registrar(Funcionario func){
         salarioTotal += func.calcularSalario();
     } 
-
+    
+    public void logar(Autenticavel aut){
+        aut.fazerLogin();
+    }
+    
     public double getSalarioTotal() {
         return salarioTotal;
     }
