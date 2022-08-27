83150e06b8c87d8c789bf6511cd9e805c64b5b72
Bruno Viana Rufino <61715911+ViFeBs@users.noreply.github.com>
2022-08-26 21:45:51 -0300

    Ajustes durante a aula

--- Nova pasta/aula03/Exercicio/funcionarios/src/FolhaDePagamento.java
+++ Nova pasta/aula03/Exercicio/funcionarios/src/FolhaDePagamento.java
@@ -2,17 +2,9 @@ public class FolhaDePagamento {
 
     private double salarioTotal = 0;
 
-    public void registrar(Engenheiro func){
+    public void registrar(Funcionario func){
         salarioTotal += func.calcularSalario();
-    }
-
-    public void registrar(Gerente func){
-        salarioTotal += func.calcularSalario();
-    }
-
-    public void registrar(Presidente func){
-        salarioTotal += func.calcularSalario();
-    }
+    } 
 
     public double getSalarioTotal() {
         return salarioTotal;
